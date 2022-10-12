6830c31e0d3d8e44ca730a54efa6d6fc210edf2a
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-12 20:50:41 -0300

    install mui core - material ui

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function FormularioCadastro(){
 
-    return(<form>
+    return(
+    <form>
+        <label>Nome</label>
         <input type="text"/>
-        </form>
+
+        <label>Sobrenome</label>
+        <input type="text"/>
+
+        <label>CPF</label>
+        <input type="text"/>
+
+        <label>Promoções</label>
+        <input type="checkbox"/>
+        <label>Novidades</label>
+        <input type="checkbox"/>
+
+        <Button type='subimit' variant="contained"> 
+            Cadastrar
+        </Button>
+    </form>
     )
 }
 
