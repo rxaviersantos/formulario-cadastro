b761e0539b1ab3e412020b27a4ffb84d33acd047
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-12 16:36:02 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro"
 
-function App() {
-  return (
-
-    <h1>Formulário de Cadastro</h1>
- 
-  );
+class  App extends Component {
+  render() {
+    return(
+      <Fragment>
+        <h1>Formulário de Cadastro</h1>
+        <FormularioCadastro/>
+      </Fragment>
+    )
+  }
 }
 
 export default App;
