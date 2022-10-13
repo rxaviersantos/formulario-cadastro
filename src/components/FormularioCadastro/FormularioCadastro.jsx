83a2099c3bd6c33573c498623f73eb70979dd309
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-12 22:17:08 -0300

    utilizando documentação lib

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField'
 
 function FormularioCadastro(){
 
     return(
     <form>
-        <label>Nome</label>
-        <input type="text"/>
-
-        <label>Sobrenome</label>
-        <input type="text"/>
-
-        <label>CPF</label>
-        <input type="text"/>
-
+        <TextField id="nome" label="Nome" variant="outlined" fullWidth />
+        <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth />
+        <TextField id="CPF" label="CPF" variant="outlined" fullWidth />
+      
         <label>Promoções</label>
         <input type="checkbox"/>
         <label>Novidades</label>
         <input type="checkbox"/>
 
-        <Button type='subimit' variant="contained"> 
+        <Button type='subimit' variant="outlined"> 
             Cadastrar
         </Button>
     </form>
