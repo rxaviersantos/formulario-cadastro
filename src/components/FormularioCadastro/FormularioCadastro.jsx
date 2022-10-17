488dd3a3e70f2adbd333439dc9cf8662bc83c924
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-16 23:52:10 -0300

    utilizando renderização condicional

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import { Typography } from '@mui/material';
+import React, { useState } from 'react';
 import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from "./DadosUsuario"
 
 
 function FormularioCadastro({Enviar, isValidCPF}){
+    const [etapaAtual, setEtapaAtual] = useState(0);
     return(
         <>
-        <DadosPessoais Enviar={Enviar} isValidCPF={isValidCPF}/>
-        <DadosUsuario/>
-        <DadosEntrega/>
+        {formularioAtual(etapaAtual)}
         </>
     )
+}
+
+function formularioAtual(etapa){
+    switch(etapa){
+        case 0:
+            return <DadosUsuario/>
+        case 1:
+            return <DadosPessoais/>
+        case 2: 
+            return  <DadosEntrega/> 
+        default:
+            return <Typography>Erro ao selecionar formulário</Typography>   
+    }
 }
 
 export default FormularioCadastro;
